bcbacac4e106bc5bc4683bfd39cffba64689a991
AhmedKhalifa01 <91398@must.edu.eg>
2023-12-17 02:26:09 +0200

    my first project

---
+++ build/urdf_testing/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ahmedkhalifa/project_ws/src/urdf_testing
+# Build directory: /home/ahmedkhalifa/project_ws/build/urdf_testing
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
